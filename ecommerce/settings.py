6b2d3457a4768cdc206d4609c4deec3912ec74a3
Akshaykr606 <akshaykr606@gmail.com>
2022-02-22 15:09:38 +0530

    css3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,6 +127,12 @@ STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'user/static')
 ]
 
+TEMPLATES_URL = 'templates/'
+
+TEMPLATESFILES_DIRS= [
+    os.path.join(BASE_DIR,'user/templates')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
